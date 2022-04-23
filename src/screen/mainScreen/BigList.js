dbf48c6f85378b981c3e3b9a3b5c05793345ae66
faruk4234 <faruk.cetin4234@gmail.com>
2022-04-24 00:32:28 +0300

    big list

--- src/screen/mainScreen/BigList.js
+++ src/screen/mainScreen/BigList.js
@@ -1,14 +1,104 @@
+/* eslint-disable import/prefer-default-export */
 import React from 'react'
-import { TouchableOpacity, Image, Text } from 'react-native'
+import {
+  TouchableOpacity,
+  Image,
+  Text,
+  ScrollView,
+  View,
+  StyleSheet
+} from 'react-native'
 import BigList from 'react-native-big-list'
-import { getTopRatedMovieApi, getImageApi } from '../../const/api'
+import { getImageApi } from '../../const/api'
 
-export const Biglist = ({ data }) => <BigList itemHeight={150} data={data} renderItem={renderItem} />
+export const Biglist = ({
+  data, page, setPage, category
+}) => (
+  <View style={styles.container}>
+
+    <Text style={styles.headerTextStyle}>
+      ----
+      {category}
+    </Text>
+
+    <ScrollView
+      horizontal
+      contentContainerStyle={styles.scroolStyle}
+    >
+      <BigList
+        bounces={false}
+        scrollEnabled={false}
+        numColumns={page * 20}
+        itemHeight={100}
+        data={data}
+        renderItem={renderItem}
+      />
+
+      <TouchableOpacity
+        style={styles.mooreButton}
+        onPress={() => setPage(page + 1)}
+      >
+        <Text style={styles.threePointStyle}>...</Text>
+
+      </TouchableOpacity>
+    </ScrollView>
+  </View>
+)
 
 const renderItem = ({ item }) => {
   return (
-    <TouchableOpacity onPress={() => console.log(item)}>
-      <Image style={{ height: 100, width: 50, borderRadius: 10 }} source={{ uri: getImageApi + item.backdrop_path }} />
+    <TouchableOpacity
+      style={styles.cardStyle}
+      onPress={() => console.log(item)}
+    >
+
+      <Image
+        style={styles.pictureStyle}
+        source={{ uri: getImageApi + item.backdrop_path }}
+      />
+
+      <Text style={styles.movieNameText}>
+        {item.original_title}
+        {' '}
+        sdads
+      </Text>
+
     </TouchableOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 70
+  },
+  headerTextStyle: {
+    fontSize: 20,
+    color: 'pink',
+    paddingLeft: 10
+  },
+  scroolStyle: {
+    height: 260
+  },
+  mooreButton: {
+    justifyContent: 'center'
+  },
+  threePointStyle: {
+    fontSize: 50,
+    marginBottom: 40
+  },
+  cardStyle: {
+    alignItems: 'center',
+    padding: 10
+  },
+  pictureStyle: {
+    height: 160,
+    width: 120,
+    borderRadius: 10,
+    paddingTop: 20
+  },
+  movieNameText: {
+    textAlign: 'center',
+    width: 120
+  }
+
+})
--- src/screen/mainScreen/index.js
+++ src/screen/mainScreen/index.js
@@ -1,28 +1,66 @@
 import React from 'react'
 import {
-  View, Text, Image, TouchableOpacity
+  View, Text, Image, TouchableOpacity, ScrollView
 } from 'react-native'
 import axios from 'axios'
-import { getTopRatedMovieApi, getImageApi } from '../../const/api'
+import { getTopRatedMovieApi, nowPlayingApi, getPopularMovieApi } from '../../const/api'
 import { Biglist } from './BigList'
 
 const SearchScreen = () => {
+
+  // top Ranked Movies
   const [dataTop, setDataTop] = React.useState(Array)
   const [topDataPage, setTopDataPage] = React.useState(1)
-
   React.useEffect(() => {
     axios
       .get(getTopRatedMovieApi + topDataPage)
       .then((res) => setDataTop([...dataTop, ...res.data.results]))
-  }, [])
+  }, [topDataPage])
+
+  // popüler Movies
+  const [dataPopüler, setDataPopüler] = React.useState(Array)
+  const [popülerDataPage, setDataPopülerPage] = React.useState(1)
+  React.useEffect(() => {
+    axios
+      .get(getPopularMovieApi + popülerDataPage)
+      .then((res) => setDataPopüler([...dataPopüler, ...res.data.results]))
+  }, [popülerDataPage])
+
+  // now playing movies
+  const [dataPlaying, setDataPlaying] = React.useState(Array)
+  const [playingPage, setPlayingPage] = React.useState(1)
+  React.useEffect(() => {
+    axios
+      .get(nowPlayingApi + playingPage)
+      .then((res) => setDataPlaying([...dataPlaying, ...res.data.results]))
+  }, [popülerDataPage])
 
   return (
-    <View style={{
-      flex: 1, justifyContent: 'center', alignItems: 'center'
-    }}
-    >
-      <Biglist data={dataTop} />
-    </View>
+    <ScrollView style={{ flex: 1 }}>
+
+      <Biglist
+        data={dataTop}
+        page={topDataPage}
+        setPage={setTopDataPage}
+        category="Top Ranked movie"
+      />
+
+      <Biglist
+        data={dataPopüler}
+        page={popülerDataPage}
+        setPage={setDataPopülerPage}
+        category="Popular"
+      />
+
+      <Biglist
+        data={dataPlaying}
+        page={playingPage}
+        setPage={setPlayingPage}
+        category="Now Playing"
+      />
+
+    </ScrollView>
+
   )
 }
 
