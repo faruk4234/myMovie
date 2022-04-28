736638b1336e455fa114952e2f14977cd305495f
faruk4234 <faruk.cetin4234@gmail.com>
2022-04-28 10:17:45 +0300

    redux sage okey

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,23 +1,46 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable default-case */
 /* eslint-disable import/prefer-default-export */
 
 const initalState = {
-  movies: []
+  favori_movies: [],
+  top_Ranked_movies: [],
+  upComing_movies: [],
+  nowPlaying_movies: [],
+  popular_movies: [],
+  searching_movies: []
 }
 
 // eslint-disable-next-line consistent-return
 export const reducer = (state = initalState, action) => {
 
   switch (action.type) {
-
   case 'ADD': {
-    // eslint-disable-next-line no-param-reassign
-    state.movies = [...state.movies, action.id]
+    state.favori_movies = [...state.favori_movies, action.id]
     return { ...state }
   }
   case 'DELETE': {
-    // eslint-disable-next-line no-param-reassign
-    state.movies = state.movies.filter((movie) => movie !== (action.id))
+    state.favori_movies = state.favori_movies.filter((movie) => movie !== (action.id))
+    return { ...state }
+  }
+  case 'ADD_TOP_RANKED': {
+    state.top_Ranked_movies = [...state.top_Ranked_movies, ...action.datas]
+    return { ...state }
+  }
+  case 'ADD_UPCOMİNG': {
+    state.upComing_movies = [...state.upComing_movies, ...action.datas]
+    return { ...state }
+  }
+  case 'ADD_POPULAR': {
+    state.popular_movies = [...state.popular_movies, ...action.datas]
+    return { ...state }
+  }
+  case 'ADD_NOWPLAYİNG': {
+    state.nowPlaying_movies = [...state.nowPlaying_movies, ...action.datas]
+    return { ...state }
+  }
+  case 'SEARCH_MOVİES': {
+    state.searching_movies = action.datas
     return { ...state }
   }
   default: {
